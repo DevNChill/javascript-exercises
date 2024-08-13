950eafe930afb511fb60ab6856a5da5114315c9c
MID3EE-ARCH <169875752+MID3EE-ARCH@users.noreply.github.com>
2024-08-13 21:49:13 +0530

    Solved 6th one until now didn't had to look at the solution however looked into mdn docs for removing elements from array one

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
-
+const leapYears = function (param1) {
+  if ((param1 % 4 == 0 && param1 % 100 != 0) || param1 % 400 == 0) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
