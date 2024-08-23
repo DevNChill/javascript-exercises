541b111e8e8b388f6b4b96c758e48eefedb17827
DevNChill <169875752+DevNChill@users.noreply.github.com>
2024-08-23 20:28:22 +0530

    solved was easy

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function (param1, param2) {
+  return param1 + param2;
 };
 
-const subtract = function() {
-	
+const subtract = function (param1, param2) {
+  return param1 - param2;
 };
 
-const sum = function() {
-	
+const sum = function (param) {
+  let totalSum = param.reduce((value, current) => value + current, 0);
+  return totalSum;
 };
 
-const multiply = function() {
-
+const multiply = function (param) {
+  let totalProduct = param.reduce((value, current) => value * current, 1);
+  return totalProduct;
 };
 
-const power = function() {
-	
+const power = function (param1, param2) {
+  i = 1;
+  let product = 1;
+  while (i <= param2) {
+    product = param1 * product;
+    i++;
+  }
+  return product;
 };
 
-const factorial = function() {
-	
+const factorial = function (param) {
+  let i = 1;
+  let product = 1;
+  while (i <= param) {
+    product *= i;
+    i++;
+  }
+  return product;
 };
 
 // Do not edit below this line
@@ -29,5 +43,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
