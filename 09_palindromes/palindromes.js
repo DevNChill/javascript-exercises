54e7c414c0fba1a2239607e477c723779c3f120c
DevNChill <169875752+DevNChill@users.noreply.github.com>
2024-08-23 21:14:52 +0530

    solved 9th one

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (param) {
+  param = param.toLowerCase().replace(/[^a-z0-9]/g, "");
+  let reverse = "";
+  for (let index = 1; index <= param.length; index++) {
+    let element = param[param.length - index];
+    reverse += element;
+  }
+  return reverse === param;
 };
 
 // Do not edit below this line
