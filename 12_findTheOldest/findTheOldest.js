5a6e4cdcf5997139caaf582eb87c22984dfeba34
DevNChill <169875752+DevNChill@users.noreply.github.com>
2024-08-23 22:40:19 +0530

    solved this chapter has been the logest hours I spent and i procrastinated a lot glad it's over

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  people = people.sort((a, b) => {
+    let ageA=(a.yearOfDeath || 2024)-a.yearOfBirth;
+    let ageB=(b.yearOfDeath || 2024)-b.yearOfBirth;
+    return ageB-ageA;
+  });
+  return people[0];
 };
 
 // Do not edit below this line
